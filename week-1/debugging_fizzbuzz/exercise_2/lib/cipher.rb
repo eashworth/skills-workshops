f1c47b4031ecbde3e81d85e7716601297adbcc0b
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-08-30 11:21:24 +0100

    Refactored to make polymorphic: replace #truthy and #equal with #compare

--- week-1/debugging_fizzbuzz/exercise_2/lib/cipher.rb
+++ week-1/debugging_fizzbuzz/exercise_2/lib/cipher.rb
@@ -1,9 +1,29 @@
 class Cipher
 
   def encode(plaintext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    p plaintext # is a string containing whole alphabet.
+    p key # is a short string of letters, some characters repeated.
+    cipher = key.chars.uniq #+ (('a'..'z').to_a - key.chars)
+    p cipher # is an array of strings (each element is a letter)
+    # ('a'...'z').to_a is an array of letters from a to y - NOT Z.
+    # From this we subtract key.chars
+    # key.chars is an array of all the letters from the string key.
+    p "does this run?" #yes
+    p ('a'...'z').to_a - key.chars # [all the letters not in key]
+    # key.chars.uniq is an array of unique letters from the string key.
+
+
     ciphertext_chars = plaintext.chars.map do |char|
+      # call chars on the string plaintext => array of letters
+      #map => collects results of block run using parameter char
       (65 + cipher.find_index(char)).chr
+      p cipher.find_index(char) # output is a series of integers, and a nil.
+      # chr is an integer method - needs a number.
+      # passing in char passes in a string (a letter)
+      # find_index tries to match this letter string to one of its elements.
+      # Its elements are letters
+      # find_index should output a number
+
     end
     ciphertext_chars.join
   end
--- week-2/making_code_simpler_through_polymorphism/lib/assertion_list.rb
+++ week-2/making_code_simpler_through_polymorphism/lib/assertion_list.rb
@@ -7,13 +7,18 @@ class AssertionList
     @assertions << assertion
   end
 
+  # def run_all_assertions
+  #   @assertions.each do |assertion|
+  #     if assertion.is_a?(TruthyAssertion)
+  #       assertion.truthy
+  #     elsif assertion.is_a?(EqualAssertion)
+  #       assertion.equal
+  #     end
+  #   end
+  # end
   def run_all_assertions
     @assertions.each do |assertion|
-      if assertion.is_a?(TruthyAssertion)
-        assertion.truthy
-      elsif assertion.is_a?(EqualAssertion)
-        assertion.equal
-      end
+      assertion.compare
     end
   end
 end
